4550e5f7b4456b75420b01c7f90f6b8f3533a473
Petteri Taro <petteritaro@hotmail.com>
2024-11-12 20:37:27 +0200

    uudet korjaukset

--- src/index.py
+++ src/index.py
@@ -9,26 +9,12 @@ def main():
     mehua.lisaa_varastoon(50.7)
     mehua.ota_varastosta(3.14)
 
-    huono = Varasto(-100.0)
-    print(huono)
-    huono = Varasto(100.0, -50.7)
-    print(huono)
-
+    Varasto(-100.0)
+    Varasto(100.0, -50.7)
     olutta.lisaa_varastoon(1000.0)
-
     mehua.lisaa_varastoon(-666.0)
-
-    print(f"Olutvarasto: {olutta}")
-    print("olutta.ota_varastosta(1000.0)")
-    saatiin = olutta.ota_varastosta(1000.0)
-    print(f"saatiin {saatiin}")
-    print(f"Olutvarasto: {olutta}")
-
-    print(f"Mehuvarasto: {mehua}")
-    print("mehua.otaVarastosta(-32.9)")
-    saatiin = mehua.ota_varastosta(-32.9)
-    print(f"saatiin {saatiin}")
-    print(f"Mehuvarasto: {mehua}")
+    olutta.ota_varastosta(1000.0)
+    mehua.ota_varastosta(-32.9)
 
 
 if __name__ == "__main__":
